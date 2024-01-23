d7387421522c7492e79d2bbecebc4b1eaf214de5
minagishl <mvxd568.co@icloud.com>
2024-01-23 09:20:59 +0900

    詳細時のボタンとスライドメニューの追加

---
+++ src/components/ProvideInformationButton.tsx
@@ -0,0 +1,17 @@
+import { FaPen } from 'react-icons/fa';
+
+interface Props {
+  [key: string]: any;
+}
+
+export default function ProvideInformationButton(props: Props): React.JSX.Element {
+  return (
+    <button
+      className="my-3 flex items-center rounded-full border border-gray-300 px-4 py-1.5 text-sm transition-colors duration-150 ease-in-out hover:bg-zinc-50 disabled:bg-zinc-200 disabled:text-zinc-900 disabled:hover:bg-zinc-200"
+      {...props}
+    >
+      <FaPen className="mr-2 pb-0.5" size="13" />
+      情報提供をする
+    </button>
+  );
+}
